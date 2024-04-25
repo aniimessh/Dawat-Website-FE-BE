6143c36cacc6ef4c96ec1d9c050c13cced5e4f96
aniimessh <animeshgupta2.002@gmail.com>
2024-04-25 19:47:14 +0530

    fix: cors updated

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://dawat-restaurant-nine.vercel.app/",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
